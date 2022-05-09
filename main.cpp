a7650da09ca0c77c6e6accda452c80d2d91dfa18
Kcenia <Kcenia4010@gmail.com>
2022-05-10 00:07:40 +0300

    multidimensional

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <functional>
 #include <vector>
 #include <chrono>
+#include "evolvent.h"
+#include "grishagin_function.h"
 #include <omp.h>
 
 #define M_PI           3.14159265358979323846
@@ -34,27 +36,49 @@ struct Spec
 	std::multimap<double, std::set<Point>::iterator> Rmap;
 };
 int n = 0;
-Point GSA(std::function<double(double)> foo, double a, double b, double r = 2.0, double epsilon = 0.001, int N = 1000)
+
+struct Result {
+	std::vector<double> y;
+	double f;
+	Result(size_t N) : f(0.0) { y.resize(N); }
+};
+Result GSA(vagrish::GrishaginFunction& foo, double* a, double* b, double r = 2.0, double epsilon = 0.001, int N = 2, int NMax = 1000)
 {
+	TEvolvent evolvent(N, 10);
+	Result result(N);
+	evolvent.SetBounds(a, b);
 	std::set<Point> w;
-	w.insert(Point(a, foo(a)));
-	w.insert(Point(b, foo(b)));
-	Point t(b, foo(b));
-	Point t_1(a, foo(a));
-	Point res = (foo(a) < foo(b)) ? t_1 : t;
+	std::vector<double> y1(N);
+	std::vector<double> y2(N);
+	double A = 0.0, B = 1.0;
+	evolvent.GetImage(A, y1.data());
+	evolvent.GetImage(B, y2.data());
+	w.insert(Point(A, foo.Calculate(y1.data())));
+	w.insert(Point(B, foo.Calculate(y2.data())));
+	Point t(B, foo.Calculate(y2.data()));
+	Point t_1(A, foo.Calculate(y1.data()));
+	result.f = (t_1.z() < t.z()) ? t_1.z() : t.z();
 	Spec spec;
-	double M = abs((foo(b) - foo(a)) / (b - a));
+	double M = abs((t.z() - t_1.z()) / (B - A));
 	spec.Mset.insert(M);
 	double m = (M > 0) ? r * M : 1;
-	double R = (m * (b - a)) + ((foo(b) - foo(a)) * (foo(b) - foo(a)) / (m * (b - a))) - 2 * (foo(b) + foo(a));
+	double R = (m * (B - A)) + ((t.z() - t_1.z()) * (t.z() - t_1.z()) / (m * (B - A))) - 2 * (t.z() + t_1.z());
 	spec.Rmap.insert(std::make_pair(R, ++w.begin()));
-	while (((t.x() - t_1.x()) > epsilon) && (n < N))
+	while (((t.x() - t_1.x()) > epsilon) && (n < NMax))
 	{
 		n++;
 
+		std::vector<double> newY(N);
+
 		double x = 0.5 * (t.x() + t_1.x()) - (t.z() - t_1.z()) / (2 * m);
-		double z = foo(x);
-		res = z < res.z() ? Point(x, z) : res;
+		evolvent.GetImage(x, newY.data());
+		double z = foo.Calculate(newY.data());
+		if (z < result.f) {
+			result.f = z;
+			for (int i = 0; i < N; ++i) {
+				result.y[i] = newY[i];
+			}
+		}
 
 		auto insert_return = w.insert(Point(x, z));
 
@@ -104,7 +128,7 @@ Point GSA(std::function<double(double)> foo, double a, double b, double r = 2.0,
 		t_1.setx(ti->x());
 		t_1.setz(ti->z());
 	}
-	return res;
+	return result;
 }
 
 double foo0(double x)
@@ -184,45 +208,35 @@ double foo8(double x)
 	return (-1) * x + sin(3 * x) - 1 * a;
 }
 
+double Paraboloid(std::vector<double> x) {
+	return 3.0 * x[0] * x[0] + 1.6 * x[1] * x[1];
+}
+
 std::vector<std::vector<double>> bounds = { {2.7, 7.5}, {-3.0, 3.0},  {0.0, 10.0}, {0.0, 1.2},
 	{-10.0, 10.0}, {2.7, 7.5}, {0.0, 4.0}, {-5.0, 5.0}, {0.0, 6.5} };
 
 int main(int argc, char* argv[])
 {
-	std::vector<std::function<double(double)>> functions({ foo0, foo1, foo2, foo3, foo4, foo5, foo6, foo7, foo8 });
-	Point point;
-	int f = 0;
-	std::function<double(double)> foo = functions[f];
-	double a = bounds[f][0];
-	double b = bounds[f][1];
-	if (argc > 1)
-	{
-		for (int i = 1; i < argc; i++)
-		{
-			if (argv[i][0] == '-')
-			{
-				if (argv[i][1] == 'f')
-				{
-					int j = 1;
-					while (argv[i][j] != '=')
-						j++;
-					int k = atoi(&argv[i][++j]) - 1;
-					foo = functions[k];
-					a = bounds[k][0];
-					b = bounds[k][1];
-				}
-			}
-		}
-	}
-	for (f = 0; f < 9; f++) {
+	std::vector<double> a(2);
+	std::vector<double> b(2);
+	int N = 2;
+	vagrish::GrishaginFunction func;
+	for (int i = 1; i < 101; i++) {
 		n = 0;
-		std::cout << "function" << f << std::endl;
+		func.SetFunctionNumber(i);
+		func.GetBounds(a.data(), b.data());
 		double st = omp_get_wtime();
-		point = GSA(functions[f], bounds[f][0], bounds[f][1], 2.5, 0.01, 10000);
+		Result res = GSA(func, a.data(), b.data(), 2.5, 0.001, 2, 10000);
 		double en = omp_get_wtime();
+		std::cout << "func " << i << std::endl;
 		std::cout << "time omp = " << en - st << std::endl;
 		std::cout << " n = " << n << std::endl;
-		std::cout << "x = " << point.x() << std::endl << "z = " << point.z() << std::endl;
+		std::cout << "f = " << res.f << std::endl;
+		std::cout << "y = ";
+		for (int i = 0; i < N; i++) { 
+			std::cout << res.y[i] << " ";
+		}
+		std::cout << std::endl << std::endl;
 	}
 	return 0;
 }
