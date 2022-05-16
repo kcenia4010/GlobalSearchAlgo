f73bde0bd45888d94751f4a8381c7af98b6a3bd9
Kcenia <Kcenia4010@gmail.com>
2022-05-16 22:59:18 +0300

    multidimensional

--- main.cpp
+++ main.cpp
@@ -5,10 +5,12 @@
 #include <functional>
 #include <vector>
 #include <thread>
+#include "evolvent.h"
 #include <omp.h>
+#include "grishagin_function.h"
 
 #define M_PI           3.14159265358979323846 
-#define NUM_ITER 10000000
+#define NUM_ITER 1000000
 
 class Point
 {
@@ -33,6 +35,19 @@ struct Spec
 	std::multiset<double> Mset;
 	std::multimap<double, std::pair<std::set<Point>::iterator, std::set<Point>::iterator>> Rmap;
 };
+
+struct Result {
+	std::vector<double> y;
+	double f;
+	Result(size_t N = 2) : f(0.0) { y.resize(N); }
+};
+
+double Calculate(vagrish::GrishaginFunction& foo, const double* y) {
+	volatile double a = 1;
+	for (size_t i = 0; i < NUM_ITER; i++)
+		a *= sin(y[0]) * sin(y[0]) + cos(y[0]) * cos(y[0]);
+	return foo.Calculate(y) * a;
+}
 
 void recalculationR(Spec& spec, std::set<Point>& w, double r, double& M, double& m) {
 	spec.Rmap.clear();
@@ -116,15 +131,17 @@ void insert_point_parallel(Spec& spec, std::set<Point>& w, Point point, double M
 }
 
 
-void search(std::pair<double, std::pair<std::set<Point>::iterator, std::set<Point>::iterator>> R,
-	std::function<double(double)> foo, double m, double M,
+void search(std::pair<double, std::pair<std::set<Point>::iterator, std::set<Point>::iterator>> R, TEvolvent& evolvent,
+	vagrish::GrishaginFunction& foo, double m, double M,
 	double& prevM, double& prevR, double& newM1, double& newM2, double& newR1, double& newR2,
 	Point& new_point, int& return_code)
 {
 	Point t = *R.second.second;
 	Point t_1 = *R.second.first;
+	std::vector<double> newY(2);
 	double x = 0.5 * (t.x() + t_1.x()) - (t.z() - t_1.z()) / (2 * m);
-	double z = foo(x);
+	evolvent.GetImage(x, newY.data());
+	double z = Calculate(foo, newY.data());
 	new_point = Point(x, z);
 	prevM = abs((t.z() - t_1.z()) / (t.x() - t_1.x()));
 	if (prevM != M)
@@ -144,20 +161,41 @@ void search(std::pair<double, std::pair<std::set<Point>::iterator, std::set<Poin
 	else
 		return_code = 1;
 }
+
+void min(TEvolvent& evolvent, vagrish::GrishaginFunction& foo, double x, Result& result) {
+	std::vector<double> newY(2);
+	evolvent.GetImage(x, newY.data());
+	double z = foo.Calculate(newY.data());
+	if (z < result.f) {
+		result.f = z;
+		for (int i = 0; i < 2; ++i) {
+			result.y[i] = newY[i];
+		}
+	}
+}
+
 int n = 0;
-Point GSA(std::function<double(double)> foo, double a, double b, double r = 2, double epsilon = 0.0001, int N = 10000)
+Result GSA(vagrish::GrishaginFunction& foo, double* a, double* b, double r = 2, double epsilon = 0.0001, int N = 2, int NMax = 10000)
 {
+	TEvolvent evolvent(N, 10);
+	Result result(N);
+	evolvent.SetBounds(a, b);
 	std::set<Point> w;
-	w.insert(Point(a, foo(a)));
-	w.insert(Point(b, foo(b)));
-	Point t(b, foo(b));
-	Point t_1(a, foo(a));
-	Point res = (foo(a) < foo(b)) ? t_1 : t;
+	std::vector<double> y1(N);
+	std::vector<double> y2(N);
+	double A = 0.0, B = 1.0;
+	evolvent.GetImage(A, y1.data());
+	evolvent.GetImage(B, y2.data());
+	w.insert(Point(A, Calculate(foo, y1.data())));
+	w.insert(Point(B, Calculate(foo, y2.data())));
+	Point t(B, Calculate(foo, y2.data()));
+	Point t_1(A, Calculate(foo, y1.data()));
+	result.f = (t_1.z() < t.z()) ? t_1.z() : t.z();
 	Spec spec;
-	double M = abs((foo(b) - foo(a)) / (b - a));
+	double M = abs((t.z() - t_1.z()) / (B - A));
 	spec.Mset.insert(M);
 	double m = (M > 0) ? r * M : 1;
-	double R = m * (b - a) + (foo(b) - foo(a)) * (foo(b) - foo(a)) / (m * (b - a)) - 2 * (foo(b) + foo(a));
+	double R = (m * (B - A)) + ((t.z() - t_1.z()) * (t.z() - t_1.z()) / (m * (B - A))) - 2 * (t.z() + t_1.z());
 	spec.Rmap.insert(std::make_pair(R, std::make_pair(w.begin(), ++w.begin())));
 	int n_thread = 4;
 	double* prevM = new double[n_thread];
@@ -170,13 +208,22 @@ Point GSA(std::function<double(double)> foo, double a, double b, double r = 2, d
 	int* return_code = new int[n_thread];
 	std::vector<std::thread> threads(n_thread);
 	int count = 0;
-	while (((t.x() - t_1.x()) > epsilon) && (n < N))
+	while (((t.x() - t_1.x()) > epsilon) && (n < NMax))
 	{
 		if (n < 4)
 		{
+			std::vector<double> newY(N);
+
 			double x = 0.5 * (t.x() + t_1.x()) - (t.z() - t_1.z()) / (2 * m);
-			double z = foo(x);
-			res = z < res.z() ? Point(x, z) : res;
+			evolvent.GetImage(x, newY.data());
+			double z = Calculate(foo, newY.data());
+			if (z < result.f) {
+				result.f = z;
+				for (int i = 0; i < N; ++i) {
+					result.y[i] = newY[i];
+				}
+			}
+
 			insert_point(spec, w, Point(x, z), M, m);
 
 			if (M != *(--spec.Mset.end()))
@@ -197,7 +244,7 @@ Point GSA(std::function<double(double)> foo, double a, double b, double r = 2, d
 			int j = 0;
 			for (auto it = std::begin(threads); it != std::end(threads); ++it)
 			{
-				*it = std::thread(search, Rmax[j], foo, m, M, std::ref(prevM[j]), std::ref(prevR[j]), std::ref(newM1[j]), std::ref(newM2[j]),
+				*it = std::thread(search, Rmax[j], std::ref(evolvent), std::ref(foo), m, M, std::ref(prevM[j]), std::ref(prevR[j]), std::ref(newM1[j]), std::ref(newM2[j]),
 					std::ref(newR1[j]), std::ref(newR2[j]), std::ref(new_point[j]), std::ref(return_code[j]));
 				j++;
 			}
@@ -215,7 +262,7 @@ Point GSA(std::function<double(double)> foo, double a, double b, double r = 2, d
 					insert_point_parallel(spec, w, new_point[i], M, m,
 						newM1[i], newM2[i], prevM[i]);
 					recalculationR(spec, w, r, M, m);
-					res = new_point[i].z() < res.z() ? new_point[i] : res;
+					min(evolvent, foo, new_point[i].x(), result);
 					break;
 				}
 			}
@@ -225,7 +272,7 @@ Point GSA(std::function<double(double)> foo, double a, double b, double r = 2, d
 				{
 					insert_point_parallel(spec, w, new_point[i], M, m,
 						newM1[i], newM2[i], prevM[i], prevR[i], newR1[i], newR2[i]);
-					res = new_point[i].z() < res.z() ? new_point[i] : res;
+					min(evolvent, foo, new_point[i].x(), result);
 				}
 			}
 			new_interval(t, t_1, spec);
@@ -240,7 +287,7 @@ Point GSA(std::function<double(double)> foo, double a, double b, double r = 2, d
 	delete[] newR2;
 	delete[] new_point;
 	delete[] return_code;
-	return res;
+	return result;
 }
 
 double foo0(double x)
@@ -325,40 +372,34 @@ std::vector<std::vector<double>> bounds = { {2.7, 7.5}, {-3.0, 3.0},  {0.0, 10.0
 
 int main(int argc, char* argv[])
 {
-	std::vector<std::function<double(double)>> functions({ foo0, foo1, foo2, foo3, foo4, foo5, foo6, foo7, foo8 });
-	Point point;
-	int f = 0;
-	std::function<double(double)> foo = functions[f];
-	double a = bounds[f][0];
-	double b = bounds[f][1];
-	if (argc > 1)
-	{
-		for (int i = 1; i < argc; i++)
-		{
-			if (argv[i][0] == '-')
-			{
-				if (argv[i][1] == 'f')
-				{
-					int j = 1;
-					while (argv[i][j] != '=')
-						j++;
-					int k = atoi(&argv[i][++j]) - 1;
-					foo = functions[k];
-					a = bounds[k][0];
-					b = bounds[k][1];
-				}
-			}
-		}
-	}
-	for (f = 0; f < 9; f++) {
+	std::vector<double> a(2);
+	std::vector<double> b(2);
+	int N = 2;
+	vagrish::GrishaginFunction func;
+	for (int i = 1; i < 6; i++) {
 		n = 0;
-		std::cout << "function" << f << std::endl;
+		func.SetFunctionNumber(i);
+		func.GetBounds(a.data(), b.data());
 		double st = omp_get_wtime();
-		point = GSA(functions[f], bounds[f][0], bounds[f][1], 2.5, 0.01, 10000);
+		Result res = GSA(func, a.data(), b.data(), 11.5, 0.01, N, 10000);
 		double en = omp_get_wtime();
+		std::cout << "func " << i << std::endl;
 		std::cout << "time omp = " << en - st << std::endl;
-		std::cout << "x = " << point.x() << std::endl << "z = " << point.z() << std::endl;
-		std::cout << "n = " << n << std::endl << std::endl;
+		std::cout << " n = " << n << std::endl;
+		std::cout << "f = " << res.f << std::endl;
+		std::cout << "y = ";
+		for (int i = 0; i < N; i++) {
+			std::cout << res.y[i] << " ";
+		}
+		std::cout << std::endl << std::endl;
+		double* point = new double[2];
+		func.GetOptimumPoint(point);
+		if ((func.GetOptimumValue() - res.f > 0.1))
+		{
+			std::cout << "fail in " << i << " res.f = " << res.f << std::endl;
+			std::cout << "res.y = " << res.y[0] << " " << res.y[1] << std::endl;
+			std::cout << std::endl << std::endl;
+		}
 	}
 	return 0;
 }
